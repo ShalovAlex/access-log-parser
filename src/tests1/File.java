a6e6e26e12d1dd01f75d354ade0fa1a4bdb6f98a
Alex Shalov <shalov-aleksey@mail.ru>
2025-11-30 20:43:24 +0300

    Task1

--- src/tests1/File.java
+++ src/tests1/File.java
@@ -3,6 +3,12 @@ package tests1;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+class TooLongLineException extends RuntimeException {
+    public TooLongLineException(String message) {
+        super(message);
+    }
+}
+
 class File {
     public void readFile(String path) {
         try {
@@ -15,7 +21,13 @@ class File {
 
             while ((line = reader.readLine()) != null) {
                 int length = line.length();
-                lineCount++;
+
+                if (length > 1024) {
+                    throw new TooLongLineException("Обнаружена строка длинной " + length + " " + (lineCount + 1) +
+                            ". Максимально допустимая длина: 1024 символа.");
+                }
+
+                lineCount ++;
                 if (length > maxLength) {
                     maxLength = length;
                 }
@@ -24,12 +36,19 @@ class File {
                 }
             }
 
-            System.out.println("Общее количество строк: " + lineCount);
-            System.out.println("Длина самой длинной строки: " + maxLength);
-            System.out.println("Длина самой короткой строки: " + minLength);
+            System.out.println("Общее количество строк в файле: " + lineCount);
+            System.out.println("Длина самой длинной строки в файле: " + maxLength);
+            System.out.println("Длина самой короткой строки в файле: " + minLength);
 
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
+
+
+
+    public static void main(String[] args) {
+        File fileProcessor = new File();
+        fileProcessor.readFile("/Users/alex/Desktop/_git/AccessLogParser/src/tests1/test");
     }
 }
