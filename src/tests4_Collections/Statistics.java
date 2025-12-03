073430d2f9d942200cea6293689e880dc18acab1
Alex Shalov <shalov-aleksey@mail.ru>
2025-12-04 00:19:23 +0300

    Task4

---
+++ src/tests4_Collections/Statistics.java
@@ -0,0 +1,46 @@
+package tests4_Collections;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Statistics {
+    private final Set<String> existingPages = new HashSet<>();
+    private final Map<String, Integer> osCount = new HashMap<>();
+    private int totalOsEntries = 0;
+
+    public void addEntry(String url, int responseCode, String operatingSystem) {
+        if (responseCode == 200) {
+            existingPages.add(url);
+        }
+
+        if (operatingSystem != null && !operatingSystem.isEmpty()) {
+            osCount.put(operatingSystem, osCount.getOrDefault(operatingSystem, 0) + 1);
+            totalOsEntries++;
+        }
+    }
+
+    public Set<String> getExistingPages() {
+        return new HashSet<>(existingPages);
+    }
+
+    public int getExistingPagesCount() {
+        return existingPages.size();
+    }
+
+    public Map<String, Double> getOsStatistics() {
+        Map<String, Double> osStatistics = new HashMap<>();
+
+        if (totalOsEntries == 0) {
+            return osStatistics;
+        }
+
+        for (Map.Entry<String, Integer> entry : osCount.entrySet()) {
+            double proportion = (double) entry.getValue() / totalOsEntries;
+            osStatistics.put(entry.getKey(), proportion);
+        }
+
+        return osStatistics;
+    }
+}
